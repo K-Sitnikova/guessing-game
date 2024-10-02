d6cc9db340e5aa127cd855c034008cf937dde7cd
Ksenia Sitnikova <k.sitnikova@DOMAIN.LOCAL>
2024-10-02 11:01:26 +0300

    feat: solve task

--- src/guessing-game.js
+++ src/guessing-game.js
@@ -1,20 +1,23 @@
 class GuessingGame {
-    constructor() {}
+    constructor() {
+    }
 
     setRange(min, max) {
-
+        this.min = min
+        this.max = max
     }
 
     guess() {
-
+        this.currentGuess = Math.round((this.min + this.max) / 2)
+        return this.currentGuess
     }
 
     lower() {
-
+        this.max = this.currentGuess
     }
 
     greater() {
-
+        this.min = this.currentGuess
     }
 }
 
